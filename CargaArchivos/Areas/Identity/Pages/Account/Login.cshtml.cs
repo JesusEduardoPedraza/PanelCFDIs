1ab9a853171f228ae582020fa3a7d1385c6890a6
JESUS EDUARDO <j_edi_24@hotmail.com>
2022-08-02 12:41:53 -0500

    modificacion al index, login

--- CargaArchivos/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CargaArchivos/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,12 +81,12 @@ namespace CargaArchivos.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                // Esto no cuenta los fallos de inicio de sesión para el bloqueo de la cuenta
+                // Para permitir que los fallos de contraseña activen el bloqueo de la cuenta, establezca lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, Input.RFC, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Usuario conectado.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
@@ -95,12 +95,12 @@ namespace CargaArchivos.Areas.Identity.Pages.Account
                 }
                 if (result.IsLockedOut)
                 {
-                    _logger.LogWarning("User account locked out.");
+                    _logger.LogWarning("Cuenta de usuario bloqueada.");
                     return RedirectToPage("./Lockout");
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Intento de inicio de sesión no válido.");
                     return Page();
                 }
             }
